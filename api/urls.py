abf1234b15a5dc8d6314798a0706eb5f73bd439c
faran <farantgh@gmail.com>
2021-03-12 19:11:54 -0500

    added the urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import QuoteView, MainPage
+from django.urls import path, include
+from .views import QuoteView, MainPage, SpecificShowQuotes, CreateQuote, EditQuote
+from rest_framework import permissions, routers
+
 app_name = 'api'
 
+router = routers.DefaultRouter()
+router.register('quote', EditQuote)
 urlpatterns = [
+	path('', MainPage.as_view()),
 	path('v1/', QuoteView.as_view()),
-	path('', MainPage.as_view())
+	path('v1/shows/<slug:slug>/', SpecificShowQuotes.as_view()),
+	path("v1/quote/add/", CreateQuote.as_view()),
+	path("v1/", include(router.urls))
 ]
