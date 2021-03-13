b248ec669af34b96f861cdd373df27bdba28228a
faran <farantgh@gmail.com>
2021-03-12 19:08:00 -0500

    added list display and prepopulated field for Role and Show models in admin panel

--- api/admin.py
+++ api/admin.py
@@ -4,7 +4,17 @@ from .models import Quote, Role, Show
 @admin.register(Quote)
 class QuoteAdmin(admin.ModelAdmin):
 	list_display = ('key', 'quote', 'show', 'role')
-admin.site.register(Show)
-admin.site.register(Role)
+	list_editable_link = ("quote",)
+	
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+	list_display = ("name", "slug")
+	prepopulated_fields = {"slug": ("name", )}
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug')
+	prepopulated_fields = {"slug": ("name", )}
+
 
 
