576e3db24b5f13893fe86f4cf3fc6b1eab001b74
faran <farantgh@gmail.com>
2021-03-12 19:10:14 -0500

    added slug fir show and role models and key for quote and related name for forign key of show in quote model

--- api/models.py
+++ api/models.py
@@ -1,28 +1,40 @@
 from django.db import models
 import random
 import string
+from django.utils.text import slugify
+
 def random_key():
 	all_digits = list(string.digits)
 	random_key = ""
-	return random_key.join(random.sample(all_digits, 10))
+	return random_key.join(random.sample(all_digits, 4))
 
 class Show(models.Model):
 	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Show, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
 
 class Role(models.Model):
 	name = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Role, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
 
 
 class Quote(models.Model):
-	key = models.CharField(default=random_key,max_length=10)
+	key = models.CharField(default=random_key,max_length=10, unique=True)
 	quote = models.CharField(max_length=500, unique=True)
-	show = models.ForeignKey(Show, on_delete=models.DO_NOTHING)
+	show = models.ForeignKey(Show, on_delete=models.DO_NOTHING, related_name="show")
 	role = models.ForeignKey(Role, on_delete=models.DO_NOTHING, related_name="role")
 
 	def __str__(self):
