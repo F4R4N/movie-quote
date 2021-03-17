f1eb9f688e43ea33153dc94eb39efeb15e2ae037
faran <farantgh@gmail.com>
2021-03-17 15:38:57 -0400

    added new features url path

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, include
-from .views import QuoteView, MainPage, SpecificShowQuotes, CreateQuote, EditQuote
+from .views import MainPage, SpecificShowQuotes, AdminQuoteView, AdminDeleteUserView, AdminEditQuoteView, AdminUserView, AdminEditUserView, UserQuoteView
 from rest_framework import permissions, routers
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register('quote', EditQuote)
+router.register('user', AdminUserView)
+# router.register("quote", AdminQuoteView)
 urlpatterns = [
 	path('', MainPage.as_view()),
-	path('v1/', QuoteView.as_view()),
+	# path('v1/', QuoteView.as_view()),
 	path('v1/shows/<slug:slug>/', SpecificShowQuotes.as_view()),
-	path("v1/quote/add/", CreateQuote.as_view()),
-	path("v1/", include(router.urls))
+	path("v1/quote/", UserQuoteView.as_view()),
+	path("v1/admin/quote/", AdminQuoteView.as_view()),
+	path("v1/admin/quote/<str:key>/", AdminEditQuoteView.as_view()),
+	path("v1/admin/", include(router.urls)),
+	path("v1/admin/user/edit/<int:pk>/", AdminEditUserView.as_view()),
+	path("v1/admin/user/delete/<int:pk>/", AdminDeleteUserView.as_view()),
 ]
