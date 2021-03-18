4f05d56c507613abeab505708df3a9ed059095ad
faran <farantgh@gmail.com>
2021-03-17 20:47:23 -0400

    added show and role edit views path in urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import MainPage, SpecificShowQuotes, AdminQuoteView, AdminDeleteUserView, AdminEditQuoteView, AdminUserView, AdminEditUserView, UserQuoteView
+from .views import MainPage, SpecificShowQuotes, AdminQuoteView, AdminDeleteUserView, AdminEditQuoteView, AdminUserView, AdminEditUserView, UserQuoteView, AdminEditShowView, AdminEditRoleView
 
 
 app_name = 'api'
@@ -9,6 +9,8 @@ urlpatterns = [
 	path("v1/quote/", UserQuoteView.as_view()),
 	path("v1/admin/quote/", AdminQuoteView.as_view()),
 	path("v1/admin/quote/<str:key>/", AdminEditQuoteView.as_view()),
+	path("v1/admin/show/edit/<slug:slug>/", AdminEditShowView.as_view()),
+	path("v1/admin/role/edit/<slug:slug>/", AdminEditRoleView.as_view()),
 	path("v1/admin/user/", AdminUserView.as_view()),
 	path("v1/admin/user/edit/<int:pk>/", AdminEditUserView.as_view()),
 	path("v1/admin/user/delete/<int:pk>/", AdminDeleteUserView.as_view()),
