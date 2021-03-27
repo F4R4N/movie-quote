d1dd8618d4a850f1e3e6ed065d865872bd7a6530
faran <farantgh@gmail.com>
2021-03-27 17:08:00 -0400

    added new featres url path

--- statistic/urls.py
+++ statistic/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import VisitsYearView, VisitsMonthView
+from .views import VisitsYearView, VisitsMonthView, VisitorsView
 
 app_name = "statistic"
 
 urlpatterns = [
 	path("admin/statistic/visits/<int:year>/", VisitsYearView.as_view()),
 	path("admin/statistic/visits/<int:year>/<int:month>/", VisitsMonthView.as_view()),
+	path("admin/statistic/visitors/<int:year>/<int:month>/<int:day>/", VisitorsView.as_view()),
 ]
