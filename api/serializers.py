1fd4e717f489a4351c00ad1e9c54da4c8856bdc7
faran <farantgh@gmail.com>
2021-03-27 17:01:18 -0400

    modified indentation level that cause error

--- api/serializers.py
+++ api/serializers.py
@@ -13,77 +13,77 @@ class QuoteSerializer(serializers.ModelSerializer):
 		model = Quote
 		fields = ("quote", 'role', "show")
 	role = serializers.CharField(source="role.name")
-    show = serializers.CharField(source="show.name")
+	show = serializers.CharField(source="show.name")
 
 class AdminAddUserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+	username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+	email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+	password1 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+	password2 = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
-    class Meta:
-        model = User
-        fields = ("id","username", "first_name", 'last_name', "email", "password2", "password1", "is_superuser", "is_active", "is_staff")
-        extra_kwargs = {
-            "first_name": {"required": True},
-            "last_name": {"required": True}
-        }
+	class Meta:
+		model = User
+		fields = ("id","username", "first_name", 'last_name', "email", "password2", "password1", "is_superuser", "is_active", "is_staff")
+		extra_kwargs = {
+			"first_name": {"required": True},
+			"last_name": {"required": True}
+		}
 
-    def validate(self, attrs):
-        if self.context['request'].user.username != "faran":
-            raise serializers.ValidationError({"detail": "you dont have permission to perform this action", "hint": "contact user 'faran'"})
-        if attrs['password2'] != attrs["password1"]:
-            raise serializers.ValidationError({"password": "password fields dont match"})
-        return attrs
+	def validate(self, attrs):
+		if self.context['request'].user.username != "faran":
+			raise serializers.ValidationError({"detail": "you dont have permission to perform this action", "hint": "contact user 'faran'"})
+		if attrs['password2'] != attrs["password1"]:
+			raise serializers.ValidationError({"password": "password fields dont match"})
+		return attrs
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            is_active=validated_data['is_active'],
-            is_superuser=validated_data['is_superuser']
-        )
-        user.set_password(validated_data["password1"])
-        user.save()
-        return user
+	def create(self, validated_data):
+		user = User.objects.create(
+			username=validated_data['username'],
+			email=validated_data['email'],
+			first_name=validated_data['first_name'],
+			last_name=validated_data['last_name'],
+			is_active=validated_data['is_active'],
+			is_superuser=validated_data['is_superuser']
+		)
+		user.set_password(validated_data["password1"])
+		user.save()
+		return user
 
 
 class ShowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Show
-        fields = ("name", "slug")
-        read_only_fields = ("slug", )
+	class Meta:
+		model = Show
+		fields = ("name", "slug")
+		read_only_fields = ("slug", )
 
 class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = ("name", "slug")
-        read_only_fields = ("slug", )
+	class Meta:
+		model = Role
+		fields = ("name", "slug")
+		read_only_fields = ("slug", )
 
 
 class AdminQuoteSerializer(serializers.ModelSerializer):
-    role = serializers.CharField(required=True)
-    show = serializers.CharField(required=True)
-    class Meta:
-        model = Quote
-        fields = ("key", "quote", "role", "show")
+	role = serializers.CharField(required=True)
+	show = serializers.CharField(required=True)
+	class Meta:
+		model = Quote
+		fields = ("key", "quote", "role", "show")
 
-    def create(self, validated_data):
-        role_name = validated_data.get("role").title()
-        show_name = validated_data.get("show").title()
-        try:
-            role = Role.objects.get(name=role_name)
-        except Role.DoesNotExist:
-            role = Role.objects.create(name=role_name)
-        try:
-            show = Show.objects.get(name=show_name)
-        except Show.DoesNotExist:
-            show = Show.objects.create(name=show_name)
-        quote = Quote.objects.create(
-            quote=validated_data['quote'],
-            show=show,
-            role=role
-        )
-        return quote
+	def create(self, validated_data):
+		role_name = validated_data.get("role").title()
+		show_name = validated_data.get("show").title()
+		try:
+			role = Role.objects.get(name=role_name)
+		except Role.DoesNotExist:
+			role = Role.objects.create(name=role_name)
+		try:
+			show = Show.objects.get(name=show_name)
+		except Show.DoesNotExist:
+			show = Show.objects.create(name=show_name)
+		quote = Quote.objects.create(
+			quote=validated_data['quote'],
+			show=show,
+			role=role
+		)
+		return quote
