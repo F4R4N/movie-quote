212bf1ebbb910d2b94697600f05b2eb74af90ce2
faran <farantgh@gmail.com>
2021-03-27 17:06:35 -0400

    added statistic.utils file and added get_client_ip and get_or_create_visit in it

---
+++ statistic/utils.py
@@ -0,0 +1,24 @@
+from django.utils import timezone
+from .models import Visit
+
+def add_or_create_visit(ip):
+	try :
+		visit = Visit.objects.get(date=timezone.now().date())
+		visit.visits += 1
+		print(visit.ips.keys)
+		if ip in visit.ips:
+			visit.ips[ip] += 1
+		else:
+			visit.ips[ip] +=1
+		visit.save()
+	except Visit.DoesNotExist as dne:
+		visit = Visit.objects.create(visits=1, ips={ip: 1})
+		visit.save()
+
+def get_client_ip(request):
+	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+	if x_forwarded_for:
+		ip = x_forwarded_for.split(',')[0]
+	else:
+		ip = request.META.get('REMOTE_ADDR')
+	return ip
