6c2efd8971abe78926fa51dd30865bb76de62e7a
Faran <farantgh@gmail.com>
2021-07-06 16:29:08 +0430

    add sample of needed env variables

---
+++ env.cmd
@@ -0,0 +1,11 @@
+:: this is a sample of environment variable use for deployment
+SET CONFIG_SECRET_KEY=secret_key
+SET CONFIG_DEBUG=0
+SET CONFIG_SECURE_SSL_REDIRECT=1
+SET CONFIG_SECURE_PROXY_SSL_HEADER=HTTP_X_FORWARDED_PROTO, https
+SET CONFIG_ALLOWED_HOSTS=127.0.0.1, localhost
+SET DB=db_name
+SET DB_USER=db_user
+SET DB_PASSWORD=db_password
+SET DB_HOST=db_hostname
+SET DB_PORT=db_port
