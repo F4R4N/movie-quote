de23ee5405d488abc117f4dab341e0d82c529a9d
Faran <farantgh@gmail.com>
2021-07-08 20:20:22 +0430

    rename ips to visitors

--- statistic/models.py
+++ statistic/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Visit(models.Model):
 	visits = models.PositiveIntegerField(default=0)
 	date = models.DateField(auto_now=True, unique=True)  # deployment
-	ips = models.JSONField(blank=True, null=True)
-	# date = models.DateField(unique=True) # development
+	visitors = models.JSONField(blank=True, null=True)
+	# date = models.DateField(unique=True)  # development
 
 	def __str__(self):
 		return "visits: '{0}', date: '{1}'".format(str(self.visits), str(self.date))
