796da4567e312654671c33ac725cff1930a60c88
Faran <farantgh@gmail.com>
2021-07-08 22:36:51 +0430

    test

--- config/report.py
+++ config/report.py
@@ -4,7 +4,7 @@ import time
 while True:
     cur_year, cur_month = datetime.datetime.now().year, datetime.datetime.now().month
     _, day_in_cur_month = calendar.monthrange(cur_year, cur_month)
-    if datetime.datetime.now().strftime("%Y%m%d%H%M%S") == datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, 8, 22, 31, 59).strftime("%Y%m%d%H%M%S"):
+    if datetime.datetime.now().strftime("%Y%m%d%H%M%S") == datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, 8, 22, 38, 59).strftime("%Y%m%d%H%M%S"):
         monthly_report()
         time.sleep(1)
     else:
