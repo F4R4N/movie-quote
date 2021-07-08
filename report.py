0eda0fb8deeba5e5170fc4125d5519b626fab1c3
Faran <farantgh@gmail.com>
2021-07-08 22:46:07 +0430

    add settings conf

--- report.py
+++ report.py
@@ -1,10 +1,15 @@
 from statistic.report import monthly_report
-import datetime, calendar
+import datetime
+import calendar
+import os
 import time
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 while True:
     cur_year, cur_month = datetime.datetime.now().year, datetime.datetime.now().month
     _, day_in_cur_month = calendar.monthrange(cur_year, cur_month)
-    if datetime.datetime.now().strftime("%Y%m%d%H%M%S") == datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, 8, 22, 43, 59).strftime("%Y%m%d%H%M%S"):
+    if datetime.datetime.now().strftime("%Y%m%d%H%M%S") == datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, 8, 22, 46, 59).strftime("%Y%m%d%H%M%S"):
         monthly_report()
         time.sleep(1)
     else:
