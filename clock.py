97aecb08beabfe33315c97eee8b1e1ac93c18a7d
Faran <farantgh@gmail.com>
2021-07-09 17:36:14 +0430

    add django setup to configure script

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management import call_command
 import os
+import django
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+django.setup()
 sched = BlockingScheduler()
 
 # @sched.scheduled_job("cron", year="*", month='*', day="last", hour=23, minutes=59)
