a53e4bca9d04082c37698d86826c641c331c564f
Faran <farantgh@gmail.com>
2021-07-09 15:42:44 +0430

    test monthly report

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from django.core.management import call_command
 sched = BlockingScheduler()
 
 # @sched.scheduled_job("cron", year="*", month='*', day="last", hour=23, minute=59)
-@sched.scheduled_job("cron", minute=1)
+@sched.scheduled_job("interval", minute=1)
 def send_report():
     call_command("monthly_report")
 
