8c654567f18dd9152f775992f6c7a2b3caf389f5
Faran <farantgh@gmail.com>
2021-07-09 16:26:12 +0430

    test

--- clock.py
+++ clock.py
@@ -1,8 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from django.core.management import call_command
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 sched = BlockingScheduler()
 
-# @sched.scheduled_job("cron", year="*", month='*', day="last", hour=23, minute=59)
+# @sched.scheduled_job("cron", year="*", month='*', day="last", hour=23, minutes=59)
 @sched.scheduled_job("interval", minutes=1)
 def send_report():
     call_command("migrate")
