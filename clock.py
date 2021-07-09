a440e6faa26790d3729442330b42143f75c13bdf
Faran <farantgh@gmail.com>
2021-07-09 18:03:18 +0430

    change cron to send report every night

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 sched = BlockingScheduler()
 
-@sched.scheduled_job("cron", year="*", month='*', day=9, hour=17, minutes=43)
+@sched.scheduled_job("cron", year="*", month='*', day="*", hour=23, minute=59)
 def send_report():
     call_command("monthly_report")
 
