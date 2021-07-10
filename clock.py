0d3cc7ad43e2366833db7b2bb49383eb087b5c42
Faran <farantgh@gmail.com>
2021-07-10 12:03:02 +0430

    test

--- clock.py
+++ clock.py
@@ -8,8 +8,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 sched = BlockingScheduler()
 
-@sched.scheduled_job("cron", year="*", month='*', day="*", hour=23, minute=59)
+
+# @sched.scheduled_job("cron", year="*", month='*', day="*", hour=8, minute=0)
+@sched.scheduled_job("interval", minutes=1)
 def send_report():
     call_command("monthly_report")
+
 
 sched.start()
