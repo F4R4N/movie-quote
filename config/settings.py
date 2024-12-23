5c9005aeff0e7dcf654b5019ddf777b77087146b
Faran <farantgh@gmail.com>
2024-12-23 15:16:01 +0330

    change db engine to mysql

--- config/settings.py
+++ config/settings.py
@@ -4,18 +4,13 @@ import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-# ipstack api access key
-IPSTACK_ACCESS_KEY = os.environ.get("IPSTACK_ACCESS_KEY")
-# email configs
-EMAIL_HOST = os.environ.get("CONFIG_EMAIL_HOST")
-EMAIL_HOST_USER = os.environ.get("CONFIG_EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("CONFIG_EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-# django configs
+
 SECRET_KEY = str(os.environ.get("CONFIG_SECRET_KEY"))
+
 DEBUG = bool(int(os.environ.get("CONFIG_DEBUG", 0)))
+
 ALLOWED_HOSTS = os.environ.get("CONFIG_ALLOWED_HOSTS", []).split(", ")
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,33 +62,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # database configs
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB"),
+        'ENGINE': "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
-        "TEST": {
-            "NAME": os.path.join(BASE_DIR, "test_db.sqlite3")
-        }
+        "PORT": int(os.environ.get("DB_PORT")),
     }
 }
+PASSWORD_VALIDATOR = 'django.contrib.auth.password_validation.'
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        UserAttributeSimilarityValidator',
+        'NAME': f'{PASSWORD_VALIDATOR}UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        MinimumLengthValidator',
+        'NAME': f'{PASSWORD_VALIDATOR}MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        CommonPasswordValidator',
+        'NAME': f'{PASSWORD_VALIDATOR}CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-        NumericPasswordValidator',
+        'NAME': f'{PASSWORD_VALIDATOR}NumericPasswordValidator',
     },
 ]
 
@@ -108,13 +97,14 @@ USE_L10N = True
 # with different timezones
 USE_TZ = False
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_RENDERER_CLASSES': [
@@ -138,9 +128,6 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-# change this when you made your user
-MAINSUPERUSER = "faran"
-
 # swagger configs
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,
@@ -152,6 +139,16 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+# ipstack api access key
+IPSTACK_ACCESS_KEY = os.environ.get("IPSTACK_ACCESS_KEY")
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get("CONFIG_EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("CONFIG_EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("CONFIG_EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
 CORS_ALLOW_ALL_ORIGINS = True
 if not DEBUG:
