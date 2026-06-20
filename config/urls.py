f0729d3f2ae143407428671974cf11abeb2ccdb3
Faran <farantgh@gmail.com>
2026-06-20 19:50:21 +0330

    move swagger config to another file, and add swagger auth urls

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import permissions
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-schema_view = get_schema_view(
-	openapi.Info(
-		title="Movie Quote Api Documentation",
-		default_version='v1',
-		description="auto created documentation by drf_yasg.",
-		contact=openapi.Contact(email="farantgh@gmail.com"),
-		license=openapi.License(name="BSD License"),
-	),
-	public=True,
-	permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('', include('api.urls')),
-	path("v1/auth/", include("auth.urls")),
-	path("v1/", include("statistic.urls")),
-	path('doc/', schema_view.with_ui(
-		'swagger', cache_timeout=0), name='schema_swagger_ui')
-
+    path("admin/", admin.site.urls),
+    path("", include("api.urls")),
+    path("v1/auth/", include("auth.urls")),
+    path("v1/", include("statistic.urls")),
+    path('api-auth/', include('rest_framework.urls')),
+    path("docs/", include("swagger_urls")),
 ]
