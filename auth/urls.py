a7f5c8a893e147c0c40c6519fa77d1a632385656
Faran <farantgh@gmail.com>
2026-06-20 20:06:51 +0330

    linted

--- auth/urls.py
+++ auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import LogoutView, UserLoginView
 from rest_framework_simplejwt.views import TokenRefreshView
+
 app_name = "auth"
 urlpatterns = [
-	path('login/', UserLoginView.as_view(), name='auth_login'),
-	path('login/refresh/', TokenRefreshView.as_view(), name='auth_refresh'),
-	path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path("login/", UserLoginView.as_view(), name="auth_login"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="auth_refresh"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
 ]
